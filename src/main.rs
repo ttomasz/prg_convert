c6b5bc15684be2743d18e8c0acc3e602741dcffd
Tomasz Taraś <tomasztaras@outlook.com>
2025-11-08 16:10:49 +0100

    add more parameters (some unused for now) and changed code to support multiple input paths to be provided

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,105 @@
+use std::path::PathBuf;
+
 use anyhow::{Context, Result};
 use arrow::csv::writer::WriterBuilder;
 use clap::Parser;
+use glob::glob;
 use quick_xml::reader::Reader;
 
 use prg_convert::AddressParser;
 
-const BATCH_SIZE: usize = 100_000;
+const DEFAULT_BATCH_SIZE: usize = 100_000;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(long = "input", help = "Input XML file path")]
-    input: std::path::PathBuf,
-    #[arg(long = "output", help = "Output file path")]
-    output: std::path::PathBuf,
-    #[arg(long = "batch-size", help = format!("How many rows are kept in memory before writing to output (default: {})", BATCH_SIZE))]
+    #[arg(
+        long = "input-paths",
+        help = "Input XML file path(s). Can be multiple paths separated with space. Can use glob patterns (e.g. `data/*.xml`).",
+        value_delimiter = ' ',
+        num_args = 1..,
+    )]
+    input_paths: Vec<String>,
+    #[arg(long = "output-path", help = "Output file path.")]
+    output_path: std::path::PathBuf,
+    #[arg(long = "output-format", help = "Output file format (one of: csv, geoparquet).")]
+    output_format: String,
+    #[arg(long = "schema-version", help = "Schema version (one of: 2012, 2021).")]
+    schema_version: String,
+    #[arg(long = "batch-size", help = format!("How many rows are kept in memory before writing to output (default: {}).", DEFAULT_BATCH_SIZE))]
     batch_size: Option<usize>,
 }
 
 fn main() -> Result<()> {
+    let start_time = std::time::Instant::now();
     let args = Cli::parse();
-    let batch_size = args.batch_size.unwrap_or(BATCH_SIZE);
+    let batch_size = args.batch_size.unwrap_or(DEFAULT_BATCH_SIZE);
+    if &args.schema_version != "2012" && &args.schema_version != "2021" {
+        anyhow::bail!("unsupported schema version `{}`, expected one of: 2012, 2021", &args.schema_version);
+    }
+    if &args.output_format != "csv" && &args.output_format != "geoparquet" {
+        anyhow::bail!("unsupported format `{}`, expected one of: csv, geoparquet", &args.output_format);
+    }
+    let mut counter = 1;
     let mut total_count = 0;
+    let mut total_size = 0;
+
+    let paths: Vec<PathBuf> = args.input_paths.clone()
+        .into_iter()
+        .flat_map(|p| {glob(&p).expect("Failed to read glob pattern")})
+        .map(|p| { p.unwrap() })
+        .collect();
 
     println!("⚙️  Parameters:");
-    println!("  Input file: {}", args.input.display());
-    println!("  Output file: {}", args.output.display());
+    if paths.len() == 1 {
+        println!("  Input file: {}", &paths[0].display());
+    } else {
+        println!("  Input patterns: {}", &args.input_paths.clone().join(" "));
+        println!("  Input files:");
+        for path in &paths {
+            println!("    - {}", path.display());
+        }
+    }
+    println!("  Output file: {}", args.output_path.display());
+    println!("  Output file format: {}", args.output_format);
+    println!("  Schema version: {}", args.schema_version);
     println!("  Batch size: {}", batch_size);
     println!("----------------------------------------");
 
-    let mut reader = Reader::from_file(&args.input)
-        .with_context(|| format!("could not read XML from file `{}`", &args.input.display()))?;
-    reader.config_mut().expand_empty_elements = true;
-    println!("⚙️  XML reader configuration: {:#?}", reader.config());
-    println!("----------------------------------------");
-
-    let output_file = std::fs::File::create(&args.output)
-        .with_context(|| format!("could not create output file `{}`", &args.output.display()))?;
-    let mut csv_writer = WriterBuilder::new().with_header(true).build(output_file);
-
-    let input_file_metadata = std::fs::metadata(&args.input)
-        .with_context(|| format!("could not get metadata for file `{}`", &args.input.display()))?;
-    if input_file_metadata.is_dir() {
-        anyhow::bail!("input path `{}` is a directory, expected a file", &args.input.display());
+    let output_file = std::fs::File::create(&args.output_path)
+        .with_context(|| format!("could not create output file `{}`", &args.output_path.display()))?;
+    if &args.output_format == "geoparquet" {
+        anyhow::bail!("geoparquet format is not yet implemented");
     }
-    let input_file_size = input_file_metadata.len();
-    let input_file_type = &args.input.extension().unwrap().to_string_lossy().to_uppercase();
-    println!("📁 Input file ({}) size: {:.2} MB", input_file_type, (input_file_size as f64 / 1024.0 / 1024.0));
-    println!("----------------------------------------");
+    let mut writer = WriterBuilder::new().with_header(true).build(output_file);
 
-    let start_time = std::time::Instant::now();
-    AddressParser::new(reader, batch_size).for_each(|batch| {
-        total_count += batch.num_rows();
-        println!("Read batch of {} addresses.", batch.num_rows());
-        csv_writer.write(&batch).expect("Failed to write CSV batch");
-    });
+    let num_files_to_process = &paths.len();
+    for path in &paths {
+        let input_file_metadata = std::fs::metadata(&path)
+            .with_context(|| format!("could not get metadata for file `{}`", &path.display()))?;
+        if input_file_metadata.is_dir() {
+            anyhow::bail!("input path `{}` is a directory, expected a file", &path.display());
+        }
+        let input_file_size = input_file_metadata.len();
+        total_size += &input_file_size;
+        let mut reader = Reader::from_file(&path)
+            .with_context(|| format!("could not read XML from file `{}`", &path.display()))?;
+        reader.config_mut().expand_empty_elements = true;
+        if counter == 1 {
+            println!("⚙️  XML reader configuration: {:#?}", reader.config());
+            println!("----------------------------------------");
+        }
+
+        println!("🪓 Processing file ({}/{}): `{}`, size: {:.2}MB.", &counter, &num_files_to_process, &path.display(), (input_file_size as f64 / 1024.0 / 1024.0));
+        AddressParser::new(reader, batch_size).for_each(|batch| {
+            total_count += batch.num_rows();
+            println!("Read batch of {} addresses.", batch.num_rows());
+            writer.write(&batch).expect("Failed to write CSV batch.");
+        });
+        counter += 1;
+    }
     let duration = start_time.elapsed();
     println!("----------------------------------------");
-    println!("📊 Total addresses read {}. Duration: {:#?}", total_count, duration);
+    println!("📊 Total addresses read {}. Duration: {:#?}. Data size: {:.2}MB.", total_count, duration, (total_size as f64 / 1024.0 / 1024.0));
 
 Ok(())
 }
